b9a093b78c0045225174a82e7ab83715ace6f594
Niall Kelly <niall_kelly@manx.net>
2022-08-04 15:44:51 +0100

    Add commented out input to test without needing to manually input

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
+    // let input = "123\n+456\n-789/123";
+
     let mut lexer = Lexer::new(input.to_string());
     let tokens = lexer.lex();
 
